79c35c7287dfba0ceae16dfe190e50ffdda4244e
Peter Young <young40@gmail.com>
2016-08-03 18:33:24 +0800

    init

---
+++ SwiftRayTracing/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftRayTracing
+//
+//  Created by Peter Young on 8/3/16.
+//  Copyright © 2016 Peter Young. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
